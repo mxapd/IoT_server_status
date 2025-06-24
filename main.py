6792aae84bc43c743234eeee1302f14ee027c78b
xam <m.porseryd@gmail.com>
2025-06-24 21:36:25 +0200

    added wifi connectivity

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-# from machine import Pin
-# #import time
+from machine import Pin, idle
+import time
 import subprocess
-# red = Pin(2, Pin.OUT)
-# green = Pin(3, Pin.OUT)
-# yellow = Pin(4, Pin.OUT)
-# i = 0
-#
-# while True:
-#     i += 1
-#     if i > 10:
-#         break
-#     green.value(1)  # ON
-#     time.sleep(0.5)
-#     green.value(0)  # OFF
-#     time.sleep(0.5)
-#
-#     yellow.value(1)  # ON
-#     time.sleep(0.5)
-#     yellow.value(0)  # OFF
-#     time.sleep(0.5)
-#
-#     red.value(1)  # ON
-#     time.sleep(0.5)
-#     red.value(0)  # OFF
-#     time.sleep(0.5)
+import network
+from secrets import WIFI_SSID, WIFI_PASSWORD
 
 
+def connect_wifi(ssid, key):
+    nets = network.wlan.scan()
+    for net in nets:
+        if net.ssid == ssid:
+            print('Network found!')
+            network.wlan.connect(net.ssid, auth=(net.sec, key), timeout=5000)
+
+            while not network.wlan.isconnected():
+                idle()  # save power while waiting
+
+            print('WLAN connection succeeded!')
+            break
+
+
+# tries to ping an ip address
+# returns 0 if it gets a response otherwise returns 1
 def ping(host):
     command = ['ping', '-c', '1', host]
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result = subprocess.run(command, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, text=True)
     if result.returncode == 0:
         print(f"{host} reachable: {result.stdout}")
         return 0
@@ -37,4 +33,32 @@ def ping(host):
         return 1
 
 
-ping('192.168.1.251')  # Replace with the desired IP address
+red = Pin(2, Pin.OUT)
+green = Pin(3, Pin.OUT)
+yellow = Pin(4, Pin.OUT)
+
+connect_wifi(WIFI_PASSWORD, WIFI_PASSWORD)
+
+ping('192.168.1.251')
+
+
+#i = 0
+
+#while True:
+#    i += 1
+#    if i > 10:
+#        break
+#    green.value(1)  # ON
+#    time.sleep(0.5)
+#    green.value(0)  # OFF
+#    time.sleep(0.5)
+#
+#    yellow.value(1)  # ON
+#    time.sleep(0.5)
+#    yellow.value(0)  # OFF
+#    time.sleep(0.5)
+#
+#    red.value(1)  # ON
+#    time.sleep(0.5)
+#    red.value(0)  # OFF
+#    time.sleep(0.5)
