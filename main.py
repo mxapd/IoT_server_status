c62a47da6ec64774546777cc460df9ddd79bb528
xam <m.porseryd@gmail.com>
2025-06-25 01:36:36 +0200

    changed main.py to be more easy to follow by using a dictionary for all the devices and lights

--- main.py
+++ main.py
@@ -3,84 +3,109 @@ import time
 from secrets import WIFI_SSID, WIFI_PASSWORD
 from lib import connect_wifi, check_host
 
-# server
-red0 = Pin(2, Pin.OUT)
-green0 = Pin(3, Pin.OUT)
-yellow0 = Pin(4, Pin.OUT)
 
-yellow0.value(0)
-red0.value(0)
-green0.value(0)
+devices = {
+    "server": {
+        "red": Pin(2, Pin.OUT),
+        "green": Pin(3, Pin.OUT),
+        "yellow": Pin(4, Pin.OUT),
+        "ip": "192.168.1.251",
+        "ports": [80, 22, 443, 8096],
+    },
+    "server_jellyfin": {
+        "red": Pin(11, Pin.OUT),
+        "green": Pin(10, Pin.OUT),
+        "ip": "192.168.1.251",
+        "ports": [8096],
+    },
+    "server_web": {
+        "red": Pin(15, Pin.OUT),
+        "green": Pin(14, Pin.OUT),
+        "ip": "192.168.1.251",
+        "ports": [80],
+    },
+    "pc": {
+        "red": Pin(6, Pin.OUT),
+        "green": Pin(7, Pin.OUT),
+        "yellow": Pin(8, Pin.OUT),
+        "ip": "192.168.1.208",
+        "ports": [22],
+    }
+}
 
-# server jellyfin
-red2 = Pin(11, Pin.OUT)
-green2 = Pin(10, Pin.OUT)
 
-# server web
-red3 = Pin(15, Pin.OUT)
-green3 = Pin(14, Pin.OUT)
-
-# pc
-red1 = Pin(6, Pin.OUT)
-green1 = Pin(7, Pin.OUT)
-yellow1 = Pin(8, Pin.OUT)
-
-yellow1.value(0)
-red1.value(0)
-green1.value(0)
-
+for device in devices.values():
+    for color in ["red", "green", "yellow"]:
+        if color in device:
+            device[color].value(0)
 
 if connect_wifi(WIFI_SSID, WIFI_PASSWORD) == 0:
-    green0.value(1)
-    time.sleep(2)
-    green0.value(0)
+    for _ in range(3):
+        devices["server"]["green"].value(1)
+        time.sleep(0.5)
+        devices["server"]["green"].value(0)
+        time.sleep(0.5)
 else:
-    red0.value(1)
-    time.sleep(2)
-    red0.value(0)
+    for _ in range(3):
+        devices["server"]["red"].value(1)
+        time.sleep(0.5)
+        devices["server"]["red"].value(0)
+        time.sleep(0.5)
     raise RuntimeError("Failed to establish network connection")
 
 while True:
     # general check for if server is up
-    if (check_host("192.168.1.251", 80)
-        or check_host("192.168.1.251", 22)
-            or check_host("192.168.1.251", 443)):
-        print("Host: 192.168.1.251 is up")
-        red0.value(0)
-        green0.value(1)
-    else:
-        green0.value(0)
-        red0.value(1)
-        print("Host: 192.168.1.251 is down or unreachable.")
+    server_up = False
+
+    for port in devices["server"]["ports"]:
+        if check_host(devices["server"]["ip"], port):
+            server_up = True
+            break
+        else:
+            server_up = False
 
     # check for if pc is up
-    if check_host("192.168.1.208", 22):
-        print("Host: 192.168.1.208 is up")
-        red1.value(0)
-        green1.value(1)
+    if check_host(devices["pc"]["ip"], devices["pc"]["ports"][0]):
+        print(f"Host: {devices['pc']['ip']} is up")
+        devices["pc"]["green"].value(1)
+        devices["pc"]["red"].value(0)
     else:
-        green1.value(0)
-        red1.value(1)
-        print("Host: 192.168.1.208 is down or unreachable.")
+        print(f"Host: {devices['pc']['ip']} is down or unreachable.")
+        devices["pc"]["green"].value(0)
+        devices["pc"]["red"].value(1)
 
-    # check for jellyfin service
-    if check_host("192.168.1.251", 8096):
-        print("Jellyfin on host: 192.168.1.251 is up")
-        red2.value(0)
-        green2.value(1)
-    else:
-        green2.value(0)
-        red2.value(1)
-        print("Jellyfin on host: 192.168.1.251 is down or unreachable.")
+    if server_up:
+        print(f"Host: {devices['server']['ip']} is up")
+        devices["server"]["green"].value(1)
+        devices["server"]["red"].value(0)
 
-    # check for personal webpage
-    if check_host("192.168.1.251", 80):
-        print("Webpage on host: 192.168.1.208 is up")
-        red3.value(0)
-        green3.value(1)
+        # check for jellyfin service
+        if check_host(devices["server_jellyfin"]["ip"], devices["server_jellyfin"]["ports"][0]):
+            print(f"Jellyfin on host: {devices['server_jellyfin']['ip']} is up")
+            devices["server_jellyfin"]["green"].value(1)
+            devices["server_jellyfin"]["red"].value(0)
+        else:
+            print(f"Jellyfin on host: {devices['server_jellyfin']['ip']} is down or unreachable.")
+            devices["server_jellyfin"]["green"].value(0)
+            devices["server_jellyfin"]["red"].value(1)
+
+        if check_host(devices["server_web"]["ip"], devices["server_web"]["ports"][0]):
+            print(f"Webpage on host: {devices['server_web']['ip']} is up")
+            devices["server_web"]["green"].value(1)
+            devices["server_web"]["red"].value(0)
+        else:
+            print(f"Webpage on host: {devices['server_web']['ip']} is down or unreachable.")
+            devices["server_web"]["green"].value(0)
+            devices["server_web"]["red"].value(1)
     else:
-        green3.value(0)
-        red3.value(1)
-        print("Webpage on host: 192.168.1.208 is down or unreachable.")
+        print(f"Host: {devices['server']['ip']} is down or unreachable.")
+        devices["server"]["green"].value(0)
+        devices["server"]["red"].value(1)
+
+        devices["server_jellyfin"]["green"].value(0)
+        devices["server_jellyfin"]["red"].value(1)
+
+        devices["server_web"]["green"].value(0)
+        devices["server_web"]["red"].value(1)
 
     time.sleep(60)
