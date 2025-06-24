91bdf06e6a667bef133f2cd242027f80a37cba6a
xam <m.porseryd@gmail.com>
2025-06-24 23:01:27 +0200

    added another computer using port 22 since no web server is running on it

--- main.py
+++ main.py
@@ -4,56 +4,45 @@ from secrets import WIFI_SSID, WIFI_PASSWORD
 from lib import connect_wifi, check_host
 
 
-red = Pin(2, Pin.OUT)
-green = Pin(3, Pin.OUT)
-yellow = Pin(4, Pin.OUT)
+red0 = Pin(2, Pin.OUT)
+green0 = Pin(3, Pin.OUT)
+yellow0 = Pin(4, Pin.OUT)
 
-yellow.value(0)
-red.value(0)
-green.value(0)
+yellow0.value(0)
+red0.value(0)
+green0.value(0)
+
+red1 = Pin(6, Pin.OUT)
+green1 = Pin(7, Pin.OUT)
+yellow1 = Pin(8, Pin.OUT)
 
 if connect_wifi(WIFI_SSID, WIFI_PASSWORD) == 0:
-    green.value(1)
+    green0.value(1)
     time.sleep(2)
-    green.value(0)
+    green0.value(0)
 else:
-    red.value(1)
+    red0.value(1)
     time.sleep(2)
-    red.value(0)
+    red0.value(0)
     raise RuntimeError("Failed to establish network connection")
 
 while True:
-    host_ip = "192.168.1.251"
-    port = 80
-
-    if check_host(host_ip, 80):
-        print(f"Host: {host_ip} is up")
-        red.value(0)
-        green.value(1)
+    if check_host("192.168.1.251", 80):
+        print("Host: 192.168.1.251 is up")
+        red0.value(0)
+        green0.value(1)
     else:
-        green.value(0)
-        red.value(1)
-        print(f"Host: {host_ip} is down or unreachable.")
+        green0.value(0)
+        red0.value(1)
+        print("Host: 192.168.1.251 is down or unreachable.")
+
+    if check_host("192.168.1.208", 22):
+        print("Host: 192.168.1.208 is up")
+        red1.value(0)
+        green1.value(1)
+    else:
+        green1.value(0)
+        red1.value(1)
+        print("Host: 192.168.1.208 is down or unreachable.")
 
     time.sleep(60)
-
-#i = 0
-
-#while True:
-#    i += 1
-#    if i > 10:
-#        break
-#    green.value(1)  # ON
-#    time.sleep(0.5)
-#    green.value(0)  # OFF
-#    time.sleep(0.5)
-#
-#    yellow.value(1)  # ON
-#    time.sleep(0.5)
-#    yellow.value(0)  # OFF
-#    time.sleep(0.5)
-#
-#    red.value(1)  # ON
-#    time.sleep(0.5)
-#    red.value(0)  # OFF
-#    time.sleep(0.5)
